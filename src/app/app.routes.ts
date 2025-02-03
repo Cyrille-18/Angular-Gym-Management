da0078f679c7b9b0831a26f0d9f6b7736bee0ac2
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-03 09:52:09 +0000

    Build:  map route to login i've map the login component to the route ""

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Routes sans layout
+    {
+        path: '',
+        component: LoginComponent,
+    },
+    // Routes avec layout
+    
+    // Redirection  en cas de routes inconnues
+    {
+        path: '**',
+        component: LoginComponent
+    }
+];

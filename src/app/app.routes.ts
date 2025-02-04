87a539955ff50bc361a9b0ff25ec0158f1e3ada8
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-04 16:31:24 +0000

    Feat: Add Route for component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     // Routes sans layout
     {
         path: '',
         component: LoginComponent,
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
     },
     // Routes avec layout
     

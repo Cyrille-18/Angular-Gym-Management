9319885918c5de1babd5a4dbe7daa85da3a7cf76
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-05 15:21:00 +0000

    Fix: correct sidebars links
    i've change sidebar links to match with routes

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,28 +1,36 @@
-// layout.component.ts
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterModule } from '@angular/router';
-
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from "../pages/home/home.component";
+import { CustomersComponent } from '../pages/customers/customers.component';
+import { HomeComponent } from '../pages/home/home.component';
 
 @Component({
   selector: 'app-layout',
+  imports: [CommonModule, CustomersComponent, HomeComponent, RouterModule],
   templateUrl: './layout.component.html',
-  standalone: true,
-  imports: [CommonModule, HomeComponent, RouterModule],
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
   isSidebarOpen = true;
+  activePage = '';
 
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Écoute les changements de navigation et met à jour activePage
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.activePage = event.urlAfterRedirects;
+      }
+    });
+  }
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
 
   shouldShowLayout(): boolean {
-    return this.router.url !== '/';
+    return this.router.url !== '/'; // Cache le layout sur la page de login
   }
 }

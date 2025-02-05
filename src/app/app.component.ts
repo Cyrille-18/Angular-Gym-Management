efb767ead5c300d52460cd73b252cfc37b2b78ed
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-05 15:19:23 +0000

    style: import componet to app to call it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from "./layout/layout.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { CustomersComponent } from './pages/customers/customers.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutComponent, HomeComponent],
+  imports: [RouterOutlet, LayoutComponent, HomeComponent,CustomersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

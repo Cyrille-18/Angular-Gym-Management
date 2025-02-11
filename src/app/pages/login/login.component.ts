5c6d8c488595ed4e501a5b235bdc7d15eb9e4744
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-11 18:39:46 +0000

    Feat: Refactor login component to standalone and implement authentication logic

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
+  standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = "";
+  password: string ="";
+  errormessage: string = "";
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login(){
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        this.authService.setToken(response.token);
+        this.router.navigate(['/app/Accueil']);
+      },
+      error: (err) => {
+        this.errormessage = err.error.message;
+      }
+    }
+  );
+  }
 }

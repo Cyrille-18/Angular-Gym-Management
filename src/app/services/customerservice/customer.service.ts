f00eadcf2237d776e7c0e69ed8f233d6c6e7d4c3
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-12 23:54:51 +0000

    Feat: Implement customer service with CRUD operations and unit tests

---
+++ src/app/services/customerservice/customer.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  private baseapiUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {
+    this.getCustomers();
+  }
+
+  getCustomers(): Observable<any[]> {
+    const token = localStorage.getItem('token'); // Récupère le token de l'utilisateur connecté
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.get<any[]>(`${this.baseapiUrl}/users/customers`, { headers });
+  }
+
+  // mettre à jour les infos d'un client
+  updateCustomer(id: number, customerData: any): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.put<any>(`${this.baseapiUrl}/users/customers/update/${id}`, customerData, { headers });
+  }
+  // suppresion d'un client par son id
+  deleteCustomer(id: number): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+  
+    return this.http.delete(`${this.baseapiUrl}/users/customers/delete/${id}`, { headers, responseType: 'text' })
+      .pipe(
+        catchError((error) => {
+          console.error('Erreur lors de la suppression du client:', error);
+          return throwError(error); // Renvoyer l'erreur pour la gestion dans le composant
+        })
+      );
+  }
+  
+}

384dfcb890770626991b1df31742193add261f57
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-13 22:38:16 +0000

    Feat: Add route for PacksComponent in application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CustomersComponent } from './pages/customers/customers.component';
 import { LayoutComponent } from './layout/layout.component';
+import { PacksComponent } from './pages/packs/packs/packs.component';
 
 export const routes: Routes = [
     // Route sans layout (Page de connexion)
@@ -17,7 +18,8 @@ export const routes: Routes = [
         component: LayoutComponent,
         children: [
             { path: 'Accueil', component: HomeComponent },
-            { path: 'Client', component: CustomersComponent }
+            { path: 'Client', component: CustomersComponent },
+            {path:'Offres', component: PacksComponent}
         ]
     },
 

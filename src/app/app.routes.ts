88bde8de313e91acf5118c2a8a36cedcdb5285a5
Cyrille-18 <cyrillepio@hotmail.com>
2025-02-14 02:12:44 +0000

    Feat: Add SubscriptionsComponent route to the application layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,28 +4,30 @@ import { HomeComponent } from './pages/home/home.component';
 import { CustomersComponent } from './pages/customers/customers.component';
 import { LayoutComponent } from './layout/layout.component';
 import { PacksComponent } from './pages/packs/packs/packs.component';
+import { SubscriptionsComponent } from './pages/subscriptions/subscriptions/subscriptions.component';
 
 export const routes: Routes = [
-    // Route sans layout (Page de connexion)
-    {
-        path: '',
-        component: LoginComponent,
-    },
+  // Route sans layout (Page de connexion)
+  {
+    path: '',
+    component: LoginComponent,
+  },
 
-    // Routes avec layout (Dashboard)
-    {
-        path: 'app',  // On change le préfixe pour éviter les conflits
-        component: LayoutComponent,
-        children: [
-            { path: 'Accueil', component: HomeComponent },
-            { path: 'Client', component: CustomersComponent },
-            {path:'Offres', component: PacksComponent}
-        ]
-    },
+  // Routes avec layout (Dashboard)
+  {
+    path: 'app', // On change le préfixe pour éviter les conflits
+    component: LayoutComponent,
+    children: [
+      { path: 'Accueil', component: HomeComponent },
+      { path: 'Client', component: CustomersComponent },
+      { path: 'Offres', component: PacksComponent },
+      { path: 'Souscriptions', component: SubscriptionsComponent },
+    ],
+  },
 
-    // Redirection pour toutes les routes inconnues
-    {
-        path: '**',
-        redirectTo: ''
-    }
+  // Redirection pour toutes les routes inconnues
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
